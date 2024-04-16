8923260f1d629d466bd6045273f19d273c34ac0d
yogendra0914 <yogensubedi1855@gmail.com>
2024-04-16 14:19:47 +0545

    added slider in detail page

---
+++ lib/views/screens/app.dart
@@ -0,0 +1,99 @@
+import 'package:flutter/material.dart';
+import 'package:recipes/views/screens/recipe.dart';
+import 'package:recipes/views/screens/recipe_detail.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    final ThemeData theme = ThemeData();
+    return MaterialApp(
+      title: 'Recipe Calculator',
+      theme: ThemeData(
+          colorScheme: theme.colorScheme.copyWith(
+            primary: Colors.grey,
+            secondary: Colors.black,
+          ),
+          fontFamily: 'Poppins'),
+      home: const MyHomePage(
+        title: 'Recipe Calculator',
+      ),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  final title;
+  const MyHomePage({
+    super.key,
+    required this.title,
+  });
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+        centerTitle: true,
+        backgroundColor: Theme.of(context).colorScheme.primary,
+      ),
+      body: SafeArea(
+        child: ListView.builder(
+          itemCount: Recipe.samples.length,
+          itemBuilder: (BuildContext context, int index) {
+            return GestureDetector(
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) {
+                      //TODO: replace return with return recipe detail()
+                      return RecipeDetail(recipe: Recipe.samples[index]);
+                    },
+                  ),
+                );
+              },
+              child: buildRecipeCard(Recipe.samples[index]),
+            );
+          },
+        ),
+      ),
+    );
+  }
+
+  Widget buildRecipeCard(Recipe recipe) {
+    return Card(
+      elevation: 5.0,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            Image(
+              image: AssetImage(recipe.imageUrl),
+            ),
+            SizedBox(
+              height: 14.0,
+            ),
+            Text(
+              recipe.label,
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.w700,
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/views/screens/recipe_detail.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+import 'package:recipes/views/screens/recipe.dart';
+
+class RecipeDetail extends StatefulWidget {
+  final Recipe recipe;
+
+  const RecipeDetail({
+    super.key,
+    required this.recipe,
+  });
+
+  @override
+  State<RecipeDetail> createState() => _RecipeDetailState();
+}
+
+//to add recipedetail state
+class _RecipeDetailState extends State<RecipeDetail> {
+  int _sliderVal = 1;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.recipe.label),
+        centerTitle: true,
+      ),
+      body: SafeArea(
+        child: Column(
+          children: [
+            SizedBox(
+              height: 300,
+              width: double.infinity,
+              child: Image(
+                image: AssetImage(widget.recipe.imageUrl),
+              ),
+            ),
+            SizedBox(
+              height: 4,
+            ),
+            Text(
+              widget.recipe.label,
+              style: TextStyle(fontSize: 18),
+            ),
+
+            //TODO: Add expanded
+            Expanded(
+              child: ListView.builder(
+                padding: const EdgeInsets.all(7),
+                itemCount: widget.recipe.ingredients.length,
+                itemBuilder: (BuildContext context, int index) {
+                  final ingredient = widget.recipe.ingredients[index];
+
+                  //TODO: Add ingredient.quantity
+                  return Text("""
+                      ${ingredient.quantity}
+                      ${ingredient.measure}
+                      ${ingredient.name}""");
+                },
+              ),
+            ),
+            Slider(
+              min: 1,
+              max: 10,
+              divisions: 9,
+              label: '${_sliderVal * widget.recipe.servings} servings',
+              value: _sliderVal.toDouble(),
+              onChanged: (newValue) {
+                setState(() {
+                  _sliderVal = newValue.round();
+                });
+              },
+              activeColor: Colors.teal,
+              inactiveColor: Colors.black,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
